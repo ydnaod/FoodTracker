87281a71c5f3a2396b43376bea0c755f49e7fe99
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-03-11 16:44:09 -0500

    first commit

---
+++ FoodTracker/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  FoodTracker
+//
+//  Created by Andy Do on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
